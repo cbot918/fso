4511924b7f5270b6c40ba97f66732fa6531f3e08
cbot918 <cbot918@gmail.com>
2023-08-29 04:54:38 +0000

    update

--- officialweb/main.go
+++ officialweb/main.go
@@ -11,9 +11,19 @@ import (
 const port = ":8081"
 
 func main() {
-	fiberapp := fiber.New()
 
-	api := i.NewAPI(fiberapp)
+	cfg, err := i.NewConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := i.NewDB(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f := fiber.New()
+	api := i.NewAPI(cfg, f, db)
 
 	log.Fatal(api.App.Listen(port))
 }
